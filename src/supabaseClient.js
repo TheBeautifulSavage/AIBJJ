7f9ab44655a6ff888f06977c02b325fc4537c781
TheBeautifulSavage <119711359+TheBeautifulSavage@users.noreply.github.com>
2025-04-11 09:42:05 -0800

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://fmgelljeozwyyagcezam.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZ2VsbGplb3p3eXlhZ2NlemFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNDc3NzUsImV4cCI6MjA1OTkyMzc3NX0.-pa_fDyLlFaPEElTTC6S2nxd6WJ_CkXtjCxgCxE2Rc0' // Replace this with your actual anon key
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
